b5026758e23be92236040cb85578a1a0dab17a75
vsmaliakou <vsmaliakou@yandex.by>
2021-08-12 10:47:44 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,16 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
     // write your solution here
+    let arr = [];
+    for(let i = 0; i < expr.length; i += 10){
+        arr.push(expr.slice(i, i + 10))
+    }
+    return arr.map(a => MORSE_TABLE[a.replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-')]).join('')
 }
 
 module.exports = {
